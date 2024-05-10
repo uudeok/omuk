a03e306594c5650509ae824b0ad90b8a11930f1a
서유덕 <awesome36@daum.net>
2024-05-11 00:21:38 +0900

    feat/add ButtonBase, Button 컴포넌트 생성

--- components/Test.tsx
+++ components/Test.tsx
@@ -4,6 +4,7 @@ import Input from './common/Input';
 import BigInput from './common/BigInput';
 import useInput from '@/hooks/useInput';
 import styled from 'styled-components';
+import Button from './common/Button';
 
 const Test = () => {
     const [value, onChangeInput, isValid] = useInput({ minLength: 3, type: 'string' });
@@ -13,6 +14,9 @@ const Test = () => {
             <Input bottomText="대&소문자, 특수기호를 포함한 7자리 이상 입력해주세요" label="비밀번호">
                 <BigInput hasError={!isValid} placeholder="비밀번호를 입력해주세요" onChange={onChangeInput} />
             </Input>
+            <Button size="lg" role="round" disabled={!isValid || !value}>
+                확인
+            </Button>
         </Self>
     );
 };
@@ -22,4 +26,7 @@ export default Test;
 const Self = styled.div`
     width: 500px;
     margin: 10px auto;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
 `;
---
+++ components/common/ButtonBase.ts
@@ -0,0 +1,120 @@
+'use client';
+
+import styled, { css } from 'styled-components';
+
+export type Role = 'round' | 'kakao' | 'cancel' | 'none';
+export type Size = 'sm' | 'lg';
+
+export const buttonRoleStyle = css<{ role?: Role }>`
+    ${({ role }) => {
+        if (role === 'round') {
+            return css`
+                border-radius: 10px;
+            `;
+        }
+
+        if (role === 'kakao') {
+            return css`
+                background: url(//storage.keepgrow.com/admin/campaign/20200611043456590.svg) no-repeat center;
+                background-position: 28px;
+                /* background-color: ${(props) => props.theme.palette.kakaoColor}; */
+                font-size: 1rem;
+                cursor: pointer;
+                margin-top: 1rem;
+                width: 100%;
+                color: black;
+                border-radius: 10px;
+                text-align: center;
+                border: none;
+                padding: 1rem;
+
+                &:hover {
+                    /* background-color: ${(props) => props.theme.palette.kakaoColor}; */
+                }
+            `;
+        }
+
+        if (role === 'cancel') {
+            return css`
+                background-color: whitesmoke;
+                color: grey;
+                border-radius: 10px;
+
+                &:hover {
+                    background-color: whitesmoke;
+                    color: black;
+                }
+            `;
+        }
+
+        if (role === 'none') {
+            return css`
+                background-color: transparent;
+                color: black;
+                font-size: 1rem;
+                justify-content: left;
+                font-weight: normal;
+
+                &:hover {
+                    /* color: ${(props) => props.theme.palette.grey}; */
+                    background-color: transparent;
+                }
+            `;
+        }
+    }}
+`;
+
+export const buttonSizeStyle = css<{ size: Size }>`
+    ${({ size }) => {
+        if (size === 'sm') {
+            return css`
+                height: 35px;
+            `;
+        }
+
+        if (size === 'lg') {
+            return css`
+                height: 45px;
+            `;
+        }
+    }}
+`;
+
+export const ButtonBase = styled.button<{ role?: Role; size: Size }>`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    border: 0 solid transparent;
+    background-color: ${(props) => props.theme.colors.mainColor};
+    /* background-color: ${(props) => props.theme.palette.iconic}; */
+    color: white;
+    cursor: pointer;
+    font-size: 17px;
+    // 요소 내의 텍스트가 줄바꿈 되지 않고 한 줄에 계속 표시
+    white-space: nowrap;
+    // 요소 내의 텍스트를 선택할 수 없도록 지정 (드래그방지)
+    user-select: none;
+    // 브라우저에서 폰트를 부드럽게 렌더링
+    -webkit-font-smoothing: antialiased;
+    /* font-family: ${(props) => props.theme.fonts.pretend}; */
+    font-weight: 600;
+    transition: color 0.1s ease-in-out, background-color 0.1s ease-in-out;
+
+    &:focus {
+        outline: none;
+    }
+
+    &:disabled {
+        opacity: 0.26;
+        cursor: not-allowed;
+    }
+
+    &:hover {
+        /* background-color: ${(props) => props.theme.palette.combi}; */
+        background-color: ${(props) => props.theme.colors.mainColorLg};
+    }
+
+    ${buttonRoleStyle};
+    ${buttonSizeStyle}
+`;
