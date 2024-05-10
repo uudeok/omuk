38124e6db3914220170f1fbebda0682e9b4ae408
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-05-10 22:27:45 +0900

    feat/add useInput 훅 생성

--- components/Input.tsx
+++ components/Input.tsx
@@ -10,9 +10,12 @@ interface InputProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 /** Input 컴포넌트는 children 으로 ReactElement 타입을 갖는다. 예시) <div>, <Component>
- * Children.only 함수는 children 이 하나의 ReactElement 인지 검증한다.
+ * Children.only 함수는 children 이 하나의 ReactElement 인지 확인하고 그 자식을 반환하는 함수이다.
+ * 이때 그 값을 child 에 할당해줬다.
+ * child.props 객체를 통해 해당 엘리먼트의 속성들에 접근할 수 있다.
+ *
  * label 태그에 useId 로 생성한 id 를 넣어준다
- * cloneElement 를 통해 Input 의 children 으로 넣어준 ReactElement 를 복제하고 새로운 id 속성을 추가해 label 과 연결시킨다.
+ * cloneElement 를 통해 children 으로 넣어준 ReactElement 를 복제하고 새로운 id 속성을 추가해 label 과 연결시킨다.
  * 즉 기존의 다른 모든 속성들을 유지한 새로운 Element 를 생성한다.
  */
 
--- components/Test.tsx
+++ components/Test.tsx
@@ -1,11 +1,17 @@
+'use client';
+
 import Input from './Input';
 import BigInput from './BigInput';
 
 const Test = () => {
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+    };
+
     return (
         <div>
             <Input bottomText="입력 가능한 최대 금액은 1,000만원 이에요." label="하하하하하">
-                <BigInput hasError={false} placeholder="금액을 입력해주세요" aria-label="amount input" />
+                <BigInput hasError={false} placeholder="금액을 입력해주세요" />
             </Input>
         </div>
     );
