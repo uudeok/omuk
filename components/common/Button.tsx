22b88484b26cf65acef2c0137104aed5bc6793a2
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-05-11 19:20:34 +0900

    rename/ BigInput > InputBase 이름 변경

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,20 +1,20 @@
-import { ButtonHTMLAttributes, Ref, forwardRef, useId } from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import { ButtonBase, Role, Size } from './ButtonBase';
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
     role?: Role;
     size: Size;
 }
 
-const Button = forwardRef(function Button(props: Props, forwardedRef: Ref<HTMLButtonElement>) {
+const Button = (props: ButtonProps) => {
     const { size, role, children, ...rest } = props;
-    const buttonId = useId();
 
     return (
-        <ButtonBase ref={forwardedRef} id={buttonId} size={size} role={role} {...rest}>
-            <span>{children}</span>
+        <ButtonBase size={size} role={role} {...rest}>
+            {children}
         </ButtonBase>
     );
-});
+};
 
 export default Button;
