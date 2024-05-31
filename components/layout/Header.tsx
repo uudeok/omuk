bc1ad50fc3fee19bac4450d39e74e66d6ae709e3
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-05-31 22:22:48 +0900

    feat/ Header 컴포넌트 생성 및 Mypage 작업 시작

---
+++ components/layout/Header.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import styles from '../../styles/header.module.css';
+import { useRouter } from 'next/navigation';
+import List, { ListRow } from '../common/List';
+import Utensils from '../../assets/utensils.svg';
+import Text from '../common/Text';
+
+const Header = () => {
+    const router = useRouter();
+
+    const handleMypage = () => {
+        /** 로그인 여부 확인 후 마이페이지 이동 */
+        router.push('/');
+    };
+
+    return (
+        <div className={styles.header}>
+            <List>
+                <ListRow
+                    left={
+                        <div>
+                            <Utensils width={18} />
+                            <Text typography="t4">OMUK</Text>
+                        </div>
+                    }
+                    right={
+                        <div className={styles.mypage} onClick={handleMypage}>
+                            MyPage
+                        </div>
+                    }
+                />
+            </List>
+        </div>
+    );
+};
+
+export default Header;
--- components/layout/SideBar.tsx
+++ components/layout/SideBar.tsx
@@ -1,16 +1,15 @@
-'use client';
-
 import Search from '../Search';
 import Card from '../Card';
 import styles from '../../styles/sideBar.module.css';
 import Controller from '../Controller';
+import Header from './Header';
 
 const SideBar = () => {
     return (
         <div className={styles.continaer}>
             <div className={styles.layout}>
                 <div className={styles.search}>
-                    <div>YamYam</div>
+                    <Header />
                     <Search />
                     <Controller />
                 </div>
