4c7d29df32bd736cf4005559748f8b123fb05519
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-06-02 22:49:39 +0900

    rename/ StyledComponent -> StyledProvider 로 수정 후, context 폴더로 이동

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -13,12 +13,6 @@ type SlideProps = {
 const Slide = ({ children, styles }: SlideProps) => {
     const router = useRouter();
 
-    /** onCloseButton
-     *  props 로 받을 시, 재사용이 가능하다는 장점이 있다.
-     *  그러나 SlideLayout 에서 API 요청을 위해 (서버에서만 호출 가능)
-     *  여기서 마우스 이벤트 핸들러 등록으로 변경
-     */
-
     const onCloseButton = () => {
         router.push('/');
     };
--- shared/context/StyledProvider.tsx
+++ shared/context/StyledProvider.tsx
@@ -3,16 +3,16 @@
 import StyledComponentsRegistry from '@/styles/registry';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@/styles/GlobalStyle';
-import { theme } from '../styles/theme';
+import { theme } from '../../styles/theme';
 import { ReactNode } from 'react';
 
-function StyledComponent({ children }: { children: ReactNode }) {
+const StyledProvider = ({ children }: { children: ReactNode }) => {
     return (
         <StyledComponentsRegistry>
             <GlobalStyle />
             <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </StyledComponentsRegistry>
     );
-}
+};
 
-export default StyledComponent;
+export default StyledProvider;
