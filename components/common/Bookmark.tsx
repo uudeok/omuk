ccc09b43cf02a4eb1608d2dee15347a684aee264
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-06-07 15:06:30 +0900

    feat/add Bookmark supabase 데이터구조 변경 post,deleteBookmark 함수 생성

--- components/common/Bookmark.tsx
+++ components/common/Bookmark.tsx
@@ -3,7 +3,7 @@
 import NonBookmark from '../../assets/nonBookmark.svg';
 import FillBookmark from '../../assets/bookmark.svg';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { checkBookmark, updateBookmark } from '@/services/bookmarkService';
+import { getBookmark, deleteBookmark, postBookmark } from '@/services/bookmarkService';
 import { useContext } from 'react';
 import { AuthContext } from '@/shared/context/AuthProvider';
 
@@ -11,30 +11,32 @@ const Bookmark = ({ res_id }: { res_id: string }) => {
     const queryClient = useQueryClient();
     const session = useContext(AuthContext);
 
-    const { data: bookmark, error } = useQuery({
+    const { data: bookmark } = useQuery({
         queryKey: ['bookmark', res_id],
-        queryFn: () => checkBookmark(res_id),
+        queryFn: () => getBookmark(res_id),
         enabled: !!session,
     });
 
     const mutation = useMutation({
-        mutationFn: (bookmarkToggle: boolean) => updateBookmark(bookmarkToggle, res_id),
-        onMutate: (bookmarkToggle: boolean) => {
-            queryClient.setQueryData(['bookmark', res_id], bookmarkToggle);
+        mutationFn: async () => {
+            if (bookmark && bookmark.length > 0) {
+                await deleteBookmark(res_id);
+            } else {
+                await postBookmark(res_id);
+            }
         },
-
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['bookmark', res_id] });
         },
     });
 
     const handleBookmarkToggle = () => {
-        mutation.mutate(!bookmark);
+        mutation.mutate();
     };
 
     return (
-        <div onClick={() => handleBookmarkToggle()}>
-            {bookmark ? <FillBookmark width={17} /> : <NonBookmark width={17} />}
+        <div onClick={handleBookmarkToggle}>
+            {bookmark && bookmark.length > 0 ? <FillBookmark width={17} /> : <NonBookmark width={17} />}
         </div>
     );
 };
