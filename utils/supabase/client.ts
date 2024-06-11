f97ab48435bde682916be782302238badf92b039
서유덕 <awesome36@daum.net>
2024-06-11 23:57:28 +0900

    refactor/ 사용자에 대한 리뷰 목록 페이지 단위로 가져오도록 수정 usePagination, useInfiniteQuery 구현

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,5 +1,9 @@
+import { Database } from '@/shared/types/supabase';
 import { createBrowserClient } from '@supabase/ssr';
 
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
 export function createClient() {
-    return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+    return createBrowserClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
 }
