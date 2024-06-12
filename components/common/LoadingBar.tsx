6923fdd855aed223df5a4f60090b07dc141b7800
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-06-12 23:15:30 +0900

    feat/add LoadingBar 생성 및 detail 페이지 loading 페이지 삽입

---
+++ components/common/LoadingBar.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import styled from 'styled-components';
+
+const LoadingBar = () => {
+    return (
+        <LoadingBarStyle>
+            <h1>Loading...</h1>
+            <span></span>
+            <span></span>
+            <span></span>
+        </LoadingBarStyle>
+    );
+};
+
+export default LoadingBar;
+
+const LoadingBarStyle = styled.div`
+    text-align: center;
+    padding: 10px;
+
+    h1 {
+        font-size: 1.3rem;
+        margin-bottom: 1rem;
+        color: var(--mainColorDk);
+    }
+
+    span {
+        display: inline-block;
+        width: 15px;
+        height: 15px;
+        background-color: gray;
+        border-radius: 50%;
+        animation: loading 1s 0s linear infinite;
+    }
+
+    span:nth-child(1) {
+        animation-delay: 0s;
+        background-color: red;
+    }
+
+    span:nth-child(2) {
+        animation-delay: 0.2s;
+        background-color: orange;
+    }
+
+    span:nth-child(3) {
+        animation-delay: 0.4s;
+        background-color: yellowgreen;
+    }
+
+    @keyframes loading {
+        0%,
+        100% {
+            opacity: 0;
+            transform: scale(0.5);
+        }
+        50% {
+            opacity: 1;
+            transform: scale(1.2);
+        }
+    }
+`;
