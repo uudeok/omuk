7f3e1918278c1556da6df46fed10e5473e8e69f6
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-06-16 19:34:17 +0900

    refactor : supabase auth ssr 작업 init

--- utils/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-import { Database } from '@/shared/types/supabase';
-import { createBrowserClient } from '@supabase/ssr';
-
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-export function createClient() {
-    return createBrowserClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
-}
