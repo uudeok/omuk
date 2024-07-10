cf8ceafee921cc23554d0bd7e70ea60e1c48b1f6
서유덕 <awesome36@daum.net>
2024-07-10 10:32:19 +0900

    fix/ build 에러 : loginPage props type 수정

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -7,7 +7,7 @@ import { Provider } from '@supabase/supabase-js';
 import { createClient } from '@/shared/lib/supabase/brower-client';
 import { useRouter } from 'next/navigation';
 
-const LoginPage = (props: { nextUrl?: string }) => {
+const LoginPage = ({ searchParams }: { searchParams: { nextUrl?: string } }) => {
     const router = useRouter();
     const supabase = createClient();
 
@@ -15,7 +15,7 @@ const LoginPage = (props: { nextUrl?: string }) => {
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: provider,
             options: {
-                redirectTo: `${location.origin}/auth/callback?next=${props.nextUrl || ''}`,
+                redirectTo: `${location.origin}/auth/callback?next=${searchParams.nextUrl || ''}`,
             },
         });
 
@@ -51,62 +51,3 @@ const LoginPage = (props: { nextUrl?: string }) => {
 };
 
 export default LoginPage;
-
-// 'use client';
-
-// import styles from '../../../styles/login.module.css';
-// import Button from '@/components/common/Button';
-// import Text from '@/components/common/Text';
-// import { Provider } from '@supabase/supabase-js';
-// import { createClient } from '@/shared/lib/supabase/brower-client';
-// import { useRouter } from 'next/navigation';
-
-// const LoginPage = (props: { nextUrl?: string }) => {
-//     const router = useRouter();
-//     const supabase = createClient();
-
-//     console.log('props', props.nextUrl);
-//     const handleLogin = async (provider: Provider) => {
-//         const { data, error } = await supabase.auth.signInWithOAuth({
-//             provider: provider,
-//             options: {
-//                 redirectTo: `${location.origin}/auth/callback?next=${props.nextUrl || ''}`,
-//             },
-//         });
-
-//         // if (data.url) {
-//         //     redirect(data.url); // use the redirect API for your server framework
-//         // }
-
-//         if (error) {
-//             throw new Error(error.message);
-//         }
-//     };
-
-//     return (
-//         <div className={styles.login}>
-//             <div className={styles.title}>
-//                 <Text typography="t2">로그인 하기</Text>
-//                 <Text typography="st3">로그인이 필요한 서비스입니다.</Text>
-//                 <Text typography="st3">소셜 로그인 및 이메일로 로그인 할 수 있습니다.</Text>
-//             </div>
-
-//             <div className={styles.loginBtn}>
-//                 <Button size="lg" role="kakao" onClick={() => handleLogin('kakao')}>
-//                     카카오 로그인
-//                 </Button>
-//                 <Button size="lg" role="google" onClick={() => handleLogin('google')}>
-//                     구글로 로그인
-//                 </Button>
-//             </div>
-
-//             <div className={styles.business}>
-//                 <Button size="sm" role="none" onClick={() => router.push('/business-login')}>
-//                     사장님 로그인
-//                 </Button>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default LoginPage;
