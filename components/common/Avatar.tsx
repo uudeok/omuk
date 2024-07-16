7103209c66d2f2db59564ea42e5e2cd2b70eb871
서유덕 <awesome36@daum.net>
2024-07-16 18:45:10 +0900

    styles/ avatar height 조절 및 rate width 조절

--- components/common/Avatar.tsx
+++ components/common/Avatar.tsx
@@ -5,7 +5,7 @@ import Text from './Text';
 const Avatar = ({ profile }: { profile: ProfileType }) => {
     return (
         <div className={styles.avatar}>
-            <img src={profile?.avatar_url} width="45px" alt="img" />
+            <img src={profile?.avatar_url} width="45px" height="45px" alt="img" />
             <Text typography="t3">{profile?.username}</Text>
         </div>
     );
