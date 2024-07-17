5143b805420e35f3eb0130cd8059f9fbe0160bba
서유덕 <awesome36@daum.net>
2024-07-17 18:43:23 +0900

    test : babel.config, polyfill.config 설정 후 빌드 테스트

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        forceSwcTransforms: true,
+    },
     images: {
         remotePatterns: [
             {
@@ -9,14 +12,48 @@ const nextConfig = {
             },
         ],
     },
-    webpack: (config) => {
+    webpack: (config, { isServer }) => {
         config.module.rules.push({
             test: /\.svg$/,
             use: ['@svgr/webpack'],
         });
+
+        if (!isServer) {
+            const originalEntry = config.entry;
+            config.entry = async () => {
+                const entries = await originalEntry();
+                if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
+                    entries['main.js'].unshift('./polyfills.js');
+                }
+                return entries;
+            };
+        }
 
         return config;
     },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 's3-omuk-images.s3.ap-northeast-2.amazonaws.com',
+//                 pathname: '/upload/**',
+//             },
+//         ],
+//     },
+//     webpack: (config) => {
+//         config.module.rules.push({
+//             test: /\.svg$/,
+//             use: ['@svgr/webpack'],
+//         });
+
+//         return config;
+//     },
+// };
+
+// export default nextConfig;
