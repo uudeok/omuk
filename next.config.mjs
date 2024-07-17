9f28b52b656f61e4f8d673e12d870710eb73010e
서유덕 <awesome36@daum.net>
2024-07-18 00:08:07 +0900

    remove : babel 관련 모듈 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -17,32 +17,6 @@ const nextConfig = {
 
         return config;
     },
-    babel: {
-        configFile: './babel.config.js',
-    },
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: 'https',
-//                 hostname: 's3-omuk-images.s3.ap-northeast-2.amazonaws.com',
-//                 pathname: '/upload/**',
-//             },
-//         ],
-//     },
-//     webpack: (config) => {
-//         config.module.rules.push({
-//             test: /\.svg$/,
-//             use: ['@svgr/webpack'],
-//         });
-
-//         return config;
-//     },
-// };
-
-// export default nextConfig;
