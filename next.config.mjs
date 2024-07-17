a384457ea78f0258412b268781ee8930f139a1cb
서유덕 <awesome36@daum.net>
2024-07-17 21:47:25 +0900

    test : babel, polyfills 파일 삭제 후 package.json browserslist 작업 후 테스트진행

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
-import path from 'path';
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -16,43 +15,30 @@ const nextConfig = {
             use: ['@svgr/webpack'],
         });
 
-        const originalEntry = config.entry;
-        config.entry = async () => {
-            const entries = await originalEntry();
-            if (entries['main.js'] && !entries['main.js'].includes(path.resolve('./polyfills.js'))) {
-                entries['main.js'].unshift(path.resolve('./polyfills.js'));
-            }
-            return entries;
-        };
-
         return config;
     },
-    experimental: {
-        forceSwcTransforms: true,
-    },
+    transpilePackages: [
+        '@aws-sdk/client-s3',
+        '@aws-sdk/lib-storage',
+        '@cloudinary/url-gen',
+        '@supabase/ssr',
+        '@supabase/supabase-js',
+        '@tanstack/react-query',
+        '@tanstack/react-query-devtools',
+        '@tanstack/react-query-next-experimental',
+        '@types/react-slick',
+        'aws-sdk',
+        'cloudinary',
+        'dayjs',
+        'dotenv',
+        'git-filter-repo',
+        'lodash',
+        'react-kakao-maps-sdk',
+        'react-slick',
+        'regenerator-runtime',
+        'slick-carousel',
+        'zustand',
+    ],
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: 'https',
-//                 hostname: 's3-omuk-images.s3.ap-northeast-2.amazonaws.com',
-//                 pathname: '/upload/**',
-//             },
-//         ],
-//     },
-//     webpack: (config) => {
-//         config.module.rules.push({
-//             test: /\.svg$/,
-//             use: ['@svgr/webpack'],
-//         });
-
-//         return config;
-//     },
-// };
-
-// export default nextConfig;
