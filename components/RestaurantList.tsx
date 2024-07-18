f9c2d27c980b207df04b7fa716467d96d53f36f7
서유덕 <awesome36@daum.net>
2024-07-18 15:03:47 +0900

    test : Next/Link 태그 및 prefetch 설정 후 최적화 테스트 진행

--- components/RestaurantList.tsx
+++ components/RestaurantList.tsx
@@ -9,6 +9,7 @@ import { useCategory, useInfiniteScroll } from '@/hooks';
 import { useEffect } from 'react';
 import { ResponseType } from '@/shared/types';
 import LoadingBar from './common/LoadingBar';
+import Link from 'next/link';
 
 const RestaurantList = () => {
     const router = useRouter();
@@ -39,20 +40,19 @@ const RestaurantList = () => {
             {errorMsg && <div className={styles.error}>{errorMsg}</div>}
 
             {resData.map((res: ResponseType) => (
-                <ListBox
-                    onClick={() => {
-                        router.push(`/${res.id}`);
-                    }}
-                    key={res.id}
-                    top={
-                        <div className={styles.information}>
-                            <Text typography="t4">{res.place_name}</Text>
-                            <Text typography="st3">{res.road_address_name}</Text>
-                            <Text typography="st3">{res.phone}</Text>
-                        </div>
-                    }
-                    bottom={''}
-                />
+                <Link key={res.id} prefetch href={`/${res.id}`}>
+                    <ListBox
+                        key={res.id}
+                        top={
+                            <div className={styles.information}>
+                                <Text typography="t4">{res.place_name}</Text>
+                                <Text typography="st3">{res.road_address_name}</Text>
+                                <Text typography="st3">{res.phone}</Text>
+                            </div>
+                        }
+                        bottom={''}
+                    />
+                </Link>
             ))}
 
             <div ref={observerEl} />
@@ -61,3 +61,67 @@ const RestaurantList = () => {
 };
 
 export default RestaurantList;
+
+// 'use client';
+
+// import styles from '../styles/components/restaurantList.module.css';
+// import List, { ListBox } from './common/List';
+// import Text from './common/Text';
+// import { useRouter } from 'next/navigation';
+// import { useMap } from '@/shared/context/MapProvider';
+// import { useCategory, useInfiniteScroll } from '@/hooks';
+// import { useEffect } from 'react';
+// import { ResponseType } from '@/shared/types';
+// import LoadingBar from './common/LoadingBar';
+
+// const RestaurantList = () => {
+//     const router = useRouter();
+//     const { searchCategory, isLoading, errorMsg } = useCategory();
+//     const { pagination, resData, map } = useMap();
+
+//     useEffect(() => {
+//         const { kakao } = window;
+//         if (!kakao || !map) return;
+
+//         kakao.maps.load(() => {
+//             searchCategory();
+//         });
+//     }, [searchCategory, map]);
+
+//     const fetchNextPage = () => {
+//         pagination?.gotoPage(pagination.current + 1);
+//     };
+
+//     const { observerEl } = useInfiniteScroll({
+//         callbackFn: fetchNextPage,
+//         hasNextPage: pagination?.hasNextPage!,
+//     });
+
+//     return (
+//         <List>
+//             {isLoading && <LoadingBar />}
+//             {errorMsg && <div className={styles.error}>{errorMsg}</div>}
+
+//             {resData.map((res: ResponseType) => (
+//                 <ListBox
+//                     onClick={() => {
+//                         router.push(`/${res.id}`);
+//                     }}
+//                     key={res.id}
+//                     top={
+//                         <div className={styles.information}>
+//                             <Text typography="t4">{res.place_name}</Text>
+//                             <Text typography="st3">{res.road_address_name}</Text>
+//                             <Text typography="st3">{res.phone}</Text>
+//                         </div>
+//                     }
+//                     bottom={''}
+//                 />
+//             ))}
+
+//             <div ref={observerEl} />
+//         </List>
+//     );
+// };
+
+// export default RestaurantList;
