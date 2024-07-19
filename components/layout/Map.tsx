f6b9b4cb981a181f2e46672f6bedf5b667b09c54
서유덕 <awesome36@daum.net>
2024-07-19 21:15:52 +0900

    refactor : total blocking time 개선을 위한 map 불러오는 로직을 useContext > Map 컴포넌트로 이동

--- components/layout/Map.tsx
+++ components/layout/Map.tsx
@@ -3,9 +3,34 @@
 import styles from '../../styles/layouts/map.module.css';
 import { useMap } from '@/shared/context/MapProvider';
 import LoadingBar from '../common/LoadingBar';
+import { useEffect, useState } from 'react';
+import { useGeoLocation } from '@/hooks';
 
 const Map = () => {
-    const { mapEl, isLoading } = useMap();
+    const { mapEl, setMap } = useMap();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const { curLocation } = useGeoLocation();
+
+    useEffect(() => {
+        const { kakao } = window;
+        if (!kakao || !mapEl?.current) return;
+        setIsLoading(true);
+
+        kakao.maps.load(() => {
+            if (!curLocation) return;
+
+            const { latitude, longitude } = curLocation;
+
+            const center = new kakao.maps.LatLng(latitude, longitude);
+            const options = {
+                center,
+                level: 3,
+            };
+            const map = new kakao.maps.Map(mapEl.current!, options);
+            setMap(map);
+            setIsLoading(false);
+        });
+    }, [curLocation]);
 
     return (
         <>
@@ -20,3 +45,26 @@ const Map = () => {
 };
 
 export default Map;
+
+// 'use client';
+
+// import styles from '../../styles/layouts/map.module.css';
+// import { useMap } from '@/shared/context/MapProvider';
+// import LoadingBar from '../common/LoadingBar';
+
+// const Map = () => {
+//     const { mapEl, isLoading } = useMap();
+
+//     return (
+//         <>
+//             {isLoading && (
+//                 <div className={styles.loadingOverlay}>
+//                     <LoadingBar status="오늘은 뭐 먹지?" />
+//                 </div>
+//             )}
+//             <div ref={mapEl} className={styles.map}></div>
+//         </>
+//     );
+// };
+
+// export default Map;
--- shared/context/MapProvider.tsx
+++ shared/context/MapProvider.tsx
@@ -1,17 +1,6 @@
 'use client';
 
-import {
-    RefObject,
-    Dispatch,
-    SetStateAction,
-    useState,
-    useRef,
-    useEffect,
-    useContext,
-    createContext,
-    ReactNode,
-} from 'react';
-import { useGeoLocation } from '@/hooks';
+import { RefObject, Dispatch, SetStateAction, useState, useRef, useContext, createContext, ReactNode } from 'react';
 import { PaginationType, ResponseType, IMarker } from '../types';
 
 type MapType = {
@@ -24,7 +13,6 @@ type MapType = {
     setMap: Dispatch<SetStateAction<kakao.maps.Map | undefined>>;
     resData: ResponseType[];
     setResData: Dispatch<SetStateAction<ResponseType[]>>;
-    isLoading: boolean;
 };
 
 const MapContext = createContext<MapType>({
@@ -37,38 +25,14 @@ const MapContext = createContext<MapType>({
     setMap: () => [],
     resData: [],
     setResData: () => [],
-    isLoading: false,
 });
 
 const MapProvider = ({ children }: { children: ReactNode }) => {
     const mapEl = useRef<HTMLDivElement>(null);
-    const { curLocation } = useGeoLocation();
     const [markers, setMarkers] = useState<IMarker[]>([]);
     const [pagination, setPagination] = useState<PaginationType | null>(null);
     const [map, setMap] = useState<kakao.maps.Map>();
     const [resData, setResData] = useState<ResponseType[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        const { kakao } = window;
-        if (!kakao) return;
-        setIsLoading(true);
-
-        kakao.maps.load(() => {
-            if (!mapEl.current || !curLocation) return;
-
-            const { latitude, longitude } = curLocation;
-
-            const center = new kakao.maps.LatLng(latitude, longitude);
-            const options = {
-                center,
-                level: 3,
-            };
-            const map = new kakao.maps.Map(mapEl.current!, options);
-            setMap(map);
-            setIsLoading(false);
-        });
-    }, [curLocation]);
 
     return (
         <MapContext.Provider
@@ -82,7 +46,6 @@ const MapProvider = ({ children }: { children: ReactNode }) => {
                 map,
                 resData,
                 setResData,
-                isLoading,
             }}
         >
             {children}
@@ -92,3 +55,98 @@ const MapProvider = ({ children }: { children: ReactNode }) => {
 
 export default MapProvider;
 export const useMap = (): MapType => useContext(MapContext);
+
+// 'use client';
+
+// import {
+//     RefObject,
+//     Dispatch,
+//     SetStateAction,
+//     useState,
+//     useRef,
+//     useEffect,
+//     useContext,
+//     createContext,
+//     ReactNode,
+// } from 'react';
+// import { useGeoLocation } from '@/hooks';
+// import { PaginationType, ResponseType, IMarker } from '../types';
+
+// type MapType = {
+//     mapEl: RefObject<HTMLDivElement> | null;
+//     markers: IMarker[];
+//     setMarkers: Dispatch<SetStateAction<IMarker[]>>;
+//     pagination: PaginationType | null;
+//     setPagination: Dispatch<SetStateAction<PaginationType | null>>;
+//     map: kakao.maps.Map | undefined;
+//     setMap: Dispatch<SetStateAction<kakao.maps.Map | undefined>>;
+//     resData: ResponseType[];
+//     setResData: Dispatch<SetStateAction<ResponseType[]>>;
+//     isLoading: boolean;
+// };
+
+// const MapContext = createContext<MapType>({
+//     mapEl: null,
+//     markers: [],
+//     setMarkers: () => [],
+//     pagination: null,
+//     setPagination: () => null,
+//     map: undefined,
+//     setMap: () => [],
+//     resData: [],
+//     setResData: () => [],
+//     isLoading: false,
+// });
+
+// const MapProvider = ({ children }: { children: ReactNode }) => {
+//     const mapEl = useRef<HTMLDivElement>(null);
+//     const { curLocation } = useGeoLocation();
+//     const [markers, setMarkers] = useState<IMarker[]>([]);
+//     const [pagination, setPagination] = useState<PaginationType | null>(null);
+//     const [map, setMap] = useState<kakao.maps.Map>();
+//     const [resData, setResData] = useState<ResponseType[]>([]);
+//     const [isLoading, setIsLoading] = useState<boolean>(true);
+
+//     useEffect(() => {
+//         const { kakao } = window;
+//         if (!kakao) return;
+//         setIsLoading(true);
+
+//         kakao.maps.load(() => {
+//             if (!mapEl.current || !curLocation) return;
+
+//             const { latitude, longitude } = curLocation;
+
+//             const center = new kakao.maps.LatLng(latitude, longitude);
+//             const options = {
+//                 center,
+//                 level: 3,
+//             };
+//             const map = new kakao.maps.Map(mapEl.current!, options);
+//             setMap(map);
+//             setIsLoading(false);
+//         });
+//     }, [curLocation]);
+
+//     return (
+//         <MapContext.Provider
+//             value={{
+//                 mapEl,
+//                 setMarkers,
+//                 markers,
+//                 pagination,
+//                 setPagination,
+//                 setMap,
+//                 map,
+//                 resData,
+//                 setResData,
+//                 isLoading,
+//             }}
+//         >
+//             {children}
+//         </MapContext.Provider>
+//     );
+// };
+
+// export default MapProvider;
+// export const useMap = (): MapType => useContext(MapContext);
