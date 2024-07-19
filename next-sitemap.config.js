bcd2d5b4c1b8ca454d12d6aca1ecf614da9caf98
서유덕 <awesome36@daum.net>
2024-07-19 19:50:15 +0900

    robots.txt 오타 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://omuk-beta.vercel.app',
-    generateRobotsTxt: true, // (optional)
+    generateRobotsTxt: false, // (optional)
     sitemapSize: 7000,
     changefreq: 'daily',
     priority: 0.7,
