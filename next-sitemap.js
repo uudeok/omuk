aa2e83f7f1a653a376404778bf79dc82a74f3637
서유덕 <awesome36@daum.net>
2024-07-19 18:08:14 +0900

    add sitemaps 생성

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://omuk-beta.vercel.app', // 사이트의 기본 URL
+    generateRobotsTxt: true, // robots.txt 파일 자동 생성
+    sitemapSize: 7000, // 사이트맵 파일당 URL 수
+    changefreq: 'daily',
+    priority: 0.7,
+};
