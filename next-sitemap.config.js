3eafd359d8804e61a711ed073de2acd3d822c71b
서유덕 <awesome36@daum.net>
2024-07-19 18:36:05 +0900

    test : 재빌드 후 sitemap 테스트 진행

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,7 @@
 module.exports = {
     siteUrl: 'https://omuk-beta.vercel.app',
     generateRobotsTxt: true, // (optional)
+    sitemapSize: 7000,
     changefreq: 'daily',
+    priority: 0.7,
 };
