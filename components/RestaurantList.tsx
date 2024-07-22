e966ad6261843c6ea75640583b1166e5eb0da8ef
서유덕 <awesome36@daum.net>
2024-07-22 19:39:23 +0900

    chore : next.config.mjs 폰트, svg 등 정작자산 캐시정책 수정

--- components/RestaurantList.tsx
+++ components/RestaurantList.tsx
@@ -50,7 +50,6 @@ const RestaurantList = () => {
                             }
                             bottom={''}
                         />
-                        //{' '}
                     </Link>
                 ))}
             </List>
