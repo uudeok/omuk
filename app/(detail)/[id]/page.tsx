0ae8c327e32c9596348ca69883366b6bb89acd7e
서유덕 <awesome36@daum.net>
2024-07-29 18:33:55 +0900

    test : [id].tsx 페이지 ISR 로 설정 후 테스트 진행 validate : 3600 설정

--- app/(detail)/[id]/page.tsx
+++ app/(detail)/[id]/page.tsx
@@ -2,13 +2,27 @@ import RestaurantDetail from '@/components/RestaurantDetail';
 import { ParamType } from '@/shared/types';
 
 export const getDetail = async (id: string) => {
-    const response = await fetch(`https://place.map.kakao.com/m/main/v/${id}/`);
+    console.log('refetch ???');
+    const response = await fetch(`https://place.map.kakao.com/m/main/v/${id}/`, {
+        next: {
+            revalidate: 3600,
+        },
+    });
 
     if (!response.ok) {
         throw new Error('데이터 가져오기 실패');
     }
     return response.json();
 };
+
+// export const getDetail = async (id: string) => {
+//     const response = await fetch(`https://place.map.kakao.com/m/main/v/${id}/`);
+
+//     if (!response.ok) {
+//         throw new Error('데이터 가져오기 실패');
+//     }
+//     return response.json();
+// };
 
 const DetailPage = async ({ params: { id } }: ParamType) => {
     const resData = await getDetail(id);
